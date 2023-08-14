47583b7e9de2006b14362daa0eb00cd4cb70428e
Aditya <ranjan.aditya2901@gmail.com>
2023-08-15 00:55:43 +0530

    Message

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import {
   AiFillLinkedin,
   AiFillYoutube,
   AiFillInstagram,
+  AiFillGithub,
 } from "react-icons/ai";
 import { BsFillMoonFill, BsFillMoonStarsFill } from "react-icons/bs";
 import { useState } from 'react';
@@ -56,14 +57,15 @@ export default function Home() {
             <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
               Web and App Developer
             </h3>
-            <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
-              Programming needs me. Join Me...
+            <p className="text-2xl py-2 dark:text-white md:text-3xl">
+              Data analyst
             </p>
             <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
               {/* <a href='https://www.instagram.com/_a__d__i__i__i_' target='_blank'><AiFillTwitterCircle /></a> */}
               <a href='https://www.linkedin.com/in/aditya-ranjan-18bb75228' target='_blank'><AiFillLinkedin /></a>
               <a href='https://www.instagram.com/_a__d__i__i__i_' target='_blank'><AiFillInstagram /></a>
               <a href='https://www.youtube.com/channel/UCD--LMTSY3o6CwqJwPA4puQ' target='_blank'><AiFillYoutube /></a>
+              <a href='https://www.github.com/aditya-2529' target='_blank'><AiFillGithub /></a>
             </div>
             <div className="flex mx-auto bg-gradient-to-b from-teal-500 rounded-full w-56 h-56 relative overflow-hidden mt-20 md:h-86 md:w-86">
               <Image src={deved} layout="fill" objectFit="cover" />
@@ -72,30 +74,32 @@ export default function Home() {
         </section>
         <section>
           <div>
-            <h3 className="text-3xl py-1 dark:text-white ">My Work Experience</h3>
+            <h3 className="text-3xl py-1 dark:text-white">My Work Experience</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a web developer.
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500">startups </span>
-              and collaborated with talanted people to create digital ideas.
+            I Aditya Ranjan , a software engineer with specialization in
+ app development , web development and in machine
+ learning. I am a good designer using many platforms like
+ Canva, Figma, etc. Programming languages I have learned
+ are Java, Python, Javascript.
             </p>
-          </div>
-          {/* <div className="lg:flex gap-10">
+          </div><p className="text-3xl py-1 dark:text-white">
+                Experiences
+              </p>
+           <div className="lg:flex gap-10">
             <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
               <Image src={design} width={100} height={100} />
               <h3 className="text-lg font-medium pt-8 pb-2  ">
-                Beautiful Designs
+                App Developer
               </h3>
               <p className="py-2">
-                Creating elegant designs suited for your needs following core
-                design theory.
+                Creating elegant Apps using Java.
               </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
+              <h4 className="py-4 text-teal-600">Tools I Use</h4>
+              <p className="text-gray-800 py-1">Java</p>
               <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
+              <p className="text-gray-800 py-1">Android Studio</p>
             </div>
+            
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
               <Image src={code} width={100} height={100} />
               <h3 className="text-lg font-medium pt-8 pb-2 ">
@@ -105,26 +109,19 @@ export default function Home() {
                 Do you have an idea for your next great website? Let's make it a
                 reality.
               </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
+              <h4 className="py-4 text-teal-600">Tools I Use</h4>
+              <p className="text-gray-800 py-1">React</p>
+              <p className="text-gray-800 py-1">Next.Js</p>
               <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
+              <p className="text-gray-800 py-1">Node Js</p>
             </div>
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
               <Image src={consulting} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2 ">Consulting</h3>
-              <p className="py-2">
-                Are you interested in feedback for your current project? I can
-                give you tips and tricks to level it up.
-              </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
+              <h3 className="text-lg font-medium pt-8 pb-2 ">Machine Learning</h3>
+              <h4 className="py-4 text-teal-600">Tools I Use</h4>
+              <p className="text-gray-800 py-1">Python</p>
             </div>
-          </div> */}
+          </div>
         </section>
         {/* <section className="py-10">
           <div>
@@ -199,6 +196,95 @@ export default function Home() {
             </div>
           </div>
         </section> */}
+        <section id="contact" className="relative">
+        <h2 className={darkMode?"text-white sm:text-4xl text-3xl mb-1 font-medium title-font":"sm:text-4xl text-3xl mb-1 font-medium title-font"}>
+            Hire Me
+          </h2>
+      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+      
+        {/* <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+          <iframe
+            width="100%"
+            height="100%"
+            title="map"
+            className="absolute inset-0"
+            frameBorder={0}
+            marginHeight={0}
+            marginWidth={0}
+            style={{ filter: "opacity(0.7)" }}
+            src="https://www.google.com/maps/embed/v1/place?q=97+warren+st+new+york+city&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+          />
+          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+            <div className="lg:w-1/2 px-6">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                ADDRESS
+              </h2>
+              <p className="mt-1">
+                97 Warren St. <br />
+                New York, NY 10007
+              </p>
+            </div>
+            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                EMAIL
+              </h2>
+              <a className="text-indigo-400 leading-relaxed">
+                reedbarger@email.com
+              </a>
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+                PHONE
+              </h2>
+              <p className="leading-relaxed">123-456-7890</p>
+            </div>
+          </div>
+        </div> */}
+        <form
+          netlify
+          name="contact"
+          className={darkMode ? "dark flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0" : "flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"}>
+          
+          <div className="relative mb-4">
+            <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="w-full shadow-lg rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div className="relative mb-4">
+            <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full shadow-lg rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div className="relative mb-4">
+            <label
+              htmlFor="message"
+              className="leading-7 text-sm text-gray-400">
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              className="w-full shadow-lg rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            Submit
+          </button>
+        </form>
+      </div>
+    </section>
       </main>
     </div>
   )
